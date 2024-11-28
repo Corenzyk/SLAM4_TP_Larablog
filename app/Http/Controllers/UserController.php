f63ced08376d240e3aee58baa163694ed2da07a2
Corenzyk K. <corentin.kuczyk49@gmail.com>
2024-11-28 14:48:27 +0100

    Ajout de la méthode de création d'article

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,4 +10,18 @@ class UserController extends Controller
     {
         return view('articles.create');
     }
+
+    public function store(Request $request)
+    {
+        // Récupération des données transmises du formulaire
+        $data = $request->only(['title', 'content', 'draft']);
+        // Récupération de l'auteur de l'article (id du user connecté)
+        $data['user_id'] = Auth::user()->id;
+        // Récupération de l'état de l'article (brouillon ou non)
+        $data['draft'] = isset($data['draft']) ? 1 : 0;
+        // Création de l'article
+        $article = Article::create($data);
+        // Redirection de l'utilisateur vers la page de ses articles
+        return redirect()->route('dashboard');
+    }
 }
